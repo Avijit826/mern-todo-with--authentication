c363cf340087922901b6bdde1113f2d25c2c584f
Avijit Das <avijitdas826@gmail.com>
2023-03-28 09:23:24 +0530

    Setup: User Functionality

--- Backend/app.js
+++ Backend/app.js
@@ -4,11 +4,13 @@ require("./config/database").connect()
 const express = require("express")
 const app = express()
 const todoRoute = require('./routes/todo.route')
+const userRoute = require('./routes/user.route')
 
 app.use(cors({ origin: true, credentials: true }))
 app.use(express.json({ extended: false }))
 
 app.use("/api/v1/todo", todoRoute)
+app.use("/api/v1", userRoute)
 
 app.get("/", (req, res) => {
     res.send("Homepage")
